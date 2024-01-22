ece61c5d77f73099a4c82ac5f4d4b6e8bacbecb4
Atlasov Ryan <dotarayanru03@gmail.com>
2024-01-23 01:39:48 +0300

    практически закончил с проектом, осталось доработать footer

--- JavaScript/scrolling.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    let menu = document.querySelector('.horizontal-menu');
-    if (window.scrollY > 50) { // 50 - отступ от начала страницы до меню
-        menu.classList.add('scrolling');
-    } else {
-        menu.classList.remove('scrolling');
-    }
-});
